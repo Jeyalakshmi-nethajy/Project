c2f3e5571f462df240a57cfdbf56e14e5b0ef2b7
Jeyalakshmi-nethajy <jeyalakshmi2992@outlook.com>
2023-06-23 11:15:10 +0000

    Update App.java

--- src/Design/App.java
+++ src/Design/App.java
@@ -4,10 +4,10 @@ public class App {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Design");
+		System.out.println("Hashcode Design");
 		
 		Employee emp1 = new Employee(100, "ABC");
-		Employee emp2 = new Employee(100, "ABC");
+		Employee emp2 = new Employee(100, "DEF");
 		
 		System.out.println(emp1.equals(emp2));
 		System.out.println(emp1.hashCode());
